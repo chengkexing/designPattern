a72186c7949653052e2be32c87afd9a55a5405c4
chengkexing <2663531074@qq.com>
2022-09-05 15:17:10 +0800

    初始化项目，以及工厂模式包构建

--- factory-module/main/java/com/etc/pattern/pojo/Man.java
+++ factory-module/main/java/com/etc/pattern/pojo/Man.java
@@ -1,6 +1,5 @@
 package com.etc.pattern.pojo;
 
-import cn.hutool.core.util.StrUtil;
 import lombok.Data;
 
 /**
@@ -8,25 +7,17 @@ import lombok.Data;
  */
 @Data
 public class Man implements Person{
-    //编号
     private String id;
-    //说明
     private String talk;
-    //身高
-    private String height;
-    //体重
-    private String weight;
 
     public Man() {
     }
     public Man(String id) {
         this.id = id;
         this.talk = id+":我是男性";
-        this.height = "185";
-        this.weight = "80";
     }
 
     public void talking() {
-        System.out.println(StrUtil.format("{} 身高{}cm 体重{}kg", this.talk, height, weight));;
+        System.out.println(this.talk);;
     }
 }
--- factory-module/main/java/com/etc/pattern/pojo/Woman.java
+++ factory-module/main/java/com/etc/pattern/pojo/Woman.java
@@ -1,6 +1,5 @@
 package com.etc.pattern.pojo;
 
-import cn.hutool.core.util.StrUtil;
 import lombok.Data;
 
 /**
@@ -12,21 +11,15 @@ public class Woman implements Person{
     private String id;
     //说明
     private String talk;
-    //身高
-    private String height;
-    //体重
-    private String weight;
 
     public Woman() {
     }
     public Woman(String id) {
         this.id = id;
         this.talk = id+":我是女性";
-        this.height = "165";
-        this.weight = "50";
     }
 
     public void talking() {
-        System.out.println(StrUtil.format("{} 身高{}cm 体重{}kg", this.talk, height, weight));;
+        System.out.println(this.talk);;
     }
 }
